067aa72a7d22cbedb501be3b83ae3b6ac5130bd4
Kalib Hackin <nagui08@hotmail.com>
2019-05-30 22:39:35 -0500

    my_each_with_index done

--- enumrable_methods.rb
+++ enumrable_methods.rb
@@ -2,7 +2,15 @@ module Enumerable
     def my_each(x)
         count = 0
         while count < x.length
-            puts yield(x[count])
+            yield(x[count])
+            count += 1
+        end
+    end
+
+    def my_each_with_index(x)
+        count = 0
+        while count < x.length
+            yield(x[count], count)
             count += 1
         end
     end
@@ -11,5 +19,13 @@ end
 include Enumerable
 
 arr = [1, 2, 3, 4]
+arr2 = ["apple", "banana", "pineapple"]
 
-Enumerable.my_each(arr) { |x| x + 1 }
+puts "Testing my_each"
+Enumerable.my_each(arr) { |x| puts x + 1 }
+# arr.each { |x| puts x + 1 }
+
+puts "Testing my_each_with_index"
+Enumerable.my_each_with_index(arr2) { |value, index| puts "#{index}. #{value}" }
+# arr2.each_with_index { |value, index| puts "#{index}. #{value}" }
+
