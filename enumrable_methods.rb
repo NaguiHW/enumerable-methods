6ca2894d6446379390cf4fabb9fa96696a4600f9
Kalib Hackin <nagui08@hotmail.com>
2019-05-30 23:14:14 -0500

    doing some changes to my_ach and my_each_with_index methods

--- enumrable_methods.rb
+++ enumrable_methods.rb
@@ -1,31 +1,40 @@
 module Enumerable
-    def my_each(x)
+
+    def my_each
+        return self unless block_given?
         count = 0
-        while count < x.length
-            yield(x[count])
+        while count < self.length
+            yield(self[count])
+            count += 1
+        end
+        self
+    end
+
+    def my_each_with_index
+        return self unless block_given?
+        count = 0
+        while count < self.length
+            yield(self[count], count)
             count += 1
         end
     end
 
-    def my_each_with_index(x)
-        count = 0
-        while count < x.length
-            yield(x[count], count)
-            count += 1
-        end
-    end
+    
 end
 
-include Enumerable
+#include Enumerable
 
 arr = [1, 2, 3, 4]
 arr2 = ["apple", "banana", "pineapple"]
+=begin
+puts "  ***  Testing my_each  ***  "
+arr.my_each { |x| puts x * 5 }
+puts "=========  This is a separator ========"
+arr.each { |x| puts x * 5 }
+=end
 
-puts "Testing my_each"
-Enumerable.my_each(arr) { |x| puts x + 1 }
-# arr.each { |x| puts x + 1 }
-
-puts "Testing my_each_with_index"
-Enumerable.my_each_with_index(arr2) { |value, index| puts "#{index}. #{value}" }
-# arr2.each_with_index { |value, index| puts "#{index}. #{value}" }
+puts "  ***  Testing my_each_with_index  ***  "
+arr2.my_each_with_index { |value, index| puts "#{index}. #{value}" }
+puts "=========  This is a separator ========"
+arr2.each_with_index { |value, index| puts "#{index}. #{value}" }
 
